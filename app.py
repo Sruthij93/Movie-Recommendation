fdd7acc2e6210e61dc01d2515c70c78f2f402439
Sruthi <sjayachandran@nevada.unr.edu>
2024-11-26 19:20:28 -0800

    details and UI Tabs

--- app.py
+++ app.py
@@ -14,10 +14,21 @@ def recommend(movie, num_recommendations):
     recommended_movie_posters = []
     for i in rec_movies_list:
         movie_id = movies.iloc[i[0]].movie_id
+        details = fetch_movie_details(movie_id)
         recommended_movie_posters.append(fetch_poster(movie_id))
-        recommended_movies.append(movies.iloc[i[0]].title)
+        recommended_movies.append(movies.iloc[i[0]].title, 'details': details)
     return recommended_movies, recommended_movie_posters    
 
+# Function to fetch movie details from TMDB API
+def fetch_movie_details(movie_id):
+    response = requests.get(f'https://api.themoviedb.org/3/movie/{movie_id}?api_key=464b2949746d666c96b76e01946ecd3c&language=en-US')
+    data = response.json()
+    return {
+        'poster': f"https://image.tmdb.org/t/p/w500/{data['poster_path']}",
+        'overview': data.get('overview', 'No overview available.'),
+        'release_date': data.get('release_date', 'Unknown'),
+        'rating': data.get('vote_average', 'N/A')
+    }
 
 def fetch_poster(movie_id):
     response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=464b2949746d666c96b76e01946ecd3c&language=en-US'.format(movie_id))
@@ -31,11 +42,13 @@ movies_list = movies['title'].values
 similarity = pickle.load(open('similarity.pkl', 'rb'))
 
 st.title("Movie Recommender System") 
+# Tabs for better UI
+tab1, tab2, tab3 = st.tabs(["🎥 Recommend Movies", "🎭 Explore by Genre", "⭐ Top Movies"])
 selected_movie_name = st.selectbox(
     'How would you like to be contacted?',
     movies_list)
 
-# if st.button('Recommend'):
+# if st.button('Recommend'):    
 #     names, posters = recommend(selected_movie_name)
     
 
